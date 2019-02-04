6df3225dfa313fd7ea32caea3fbe6a69237a9491
John Gillott <jsgtwenty@gmail.com>
2019-02-04 10:17:47 +0000

    init script to install gnu stow

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -uo pipefail
+
+
+# Check for GNU stow and attempt to install if it doesn't exist
+stow --version >/dev/null 2>&1
+if [ $? -gt 0 ]; then
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        if dnf --version >/dev/null 2>&1; then
+            # package manager for Red Hat distros i.e. Fedora
+            echo "installing: stow via dnf"
+            sudo dnf install stow
+        elif apt >/dev/null 2>&1; then
+            # package manager for other linux distros i.e. Ubuntu
+            echo "installing: stow via apt"
+            sudo apt update
+            sudo apt install stow
+        else
+            >&2 echo "unable to determine linux distribution package manager"
+            exit 4
+        fi
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Installing: stow"
+        brew update
+        brew install stow
+    else
+        echo 'GNU Stow must be installed...'
+        exit 127
+    fi
+fi
